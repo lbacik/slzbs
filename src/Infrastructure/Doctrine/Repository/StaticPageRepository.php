bdd876e5328bad37012397439caa731e4a05ec43
Łukasz Bacik <mail@luka.sh>
2020-01-05 12:51:48 +0100

    [SLZBS-9] Added entity and repository definition for StaticPage (doctrine)

---
+++ src/Infrastructure/Doctrine/Repository/StaticPageRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Repository;
+
+use App\Entity\StaticPage as StaticPageEntity;
+use App\Service\StaticPageRepository as StaticPageRepositoryInterface;
+
+class StaticPageRepository extends GenericRepository implements StaticPageRepositoryInterface
+{
+    public function get(int $id): ?StaticPageEntity
+    {
+        $page = $this->getEntityManager()
+            ->find (
+                StaticPageEntity::class,
+                $id
+            );
+
+        return $page;
+    }
+}
