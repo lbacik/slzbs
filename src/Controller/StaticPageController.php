631ad749b8e78b41af566e59723958baced07a22
Łukasz Bacik <mail@luka.sh>
2020-01-05 12:24:24 +0100

    [SLZBS-8] Updated StaticPage controller

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\StaticPage;
+use App\Service\StaticPage\Exception\StaticPageException;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,21 +18,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class StaticPageController extends AbstractController
 {
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @var StaticPage
      */
     private $staticPage;
 
-    public function __construct(StaticPage $staticPage)
-    {
+    public function __construct(
+        LoggerInterface $logger,
+        StaticPage $staticPage
+    ) {
+        $this->logger = $logger;
         $this->staticPage = $staticPage;
     }
 
     /**
      * @Route("/show/{id}", name="static_page_show")
      */
-    public function show(string $id): Response
+    public function show(int $id): Response
     {
-        $page = $this->staticPage->get($id);
+        try {
+            $page = $this->staticPage->get($id);
+        } catch (StaticPageException $exception) {
+            $this->logger->error($exception->getMessage());
+            throw new HttpException(404);
+        }
 
         return $this->render(
             'static/show.html.twig',
