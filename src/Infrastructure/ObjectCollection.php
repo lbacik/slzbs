9045f695479560a35a4d32a8dfc0ca551a2e1319
Łukasz Bacik <mail@luka.sh>
2020-01-19 12:15:40 +0100

    [SLZBS-12] Object collection (immutable)

---
+++ src/Infrastructure/ObjectCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use Sushi\Validator\IlluminateValidationValidator;
+use Sushi\Validator\KeysValidator;
+use Sushi\ValueObject;
+
+abstract class ObjectCollection extends ValueObject
+{
+    protected const ITEMS = 'items';
+
+    protected $validators = [
+        KeysValidator::class,
+        IlluminateValidationValidator::class,
+    ];
+
+    public function item(): iterable
+    {
+        foreach($this->getValues()[self::ITEMS] as $item) {
+            yield $item;
+        }
+    }
+}
