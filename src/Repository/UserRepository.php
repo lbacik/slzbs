65c25e2f0cbb3de8ef568857ec96099b043ea503
Łukasz Bacik <mail@luka.sh>
2020-02-16 11:22:20 +0100

    [SLZBS-11] User entity and repository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function add(string $email, string $password): void
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+}
